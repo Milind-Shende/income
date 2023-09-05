3e3428bccd953c194ae49928968761f82a14c6ad
Milind Shende <milind.shende24@rediffmail.com>
2023-09-05 13:24:50 +0530

    updated streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -35,7 +35,7 @@ TRANSFORMER_DIR= os.path.join(ROOT_DIR, SAVED_DIR_PATH,SAVED_ZERO_FILE,TRANSFORM
 # transfomer=joblib.load(TRANSFORMER_DIR)
 # # print(transfomer)
 
-@st.cache
+
 def load_model():
     # Load the Model.pkl, Transformer.pkl, and Target.pkl
     model = joblib.load(MODEL_DIR)
@@ -191,7 +191,6 @@ pages = {
 
 # Streamlit application
 def main():
-    st.cache.clear() 
     # Sidebar navigation
     st.sidebar.title('Navigation')
     selected_page = st.sidebar.radio('Go to', list(pages.keys()))
